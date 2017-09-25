9d365000f9cbc437b1dc505e28594cc25eccd68d
Aaron Lun <alun@wehi.edu.au>
2017-09-25 09:22:52 +0100

    Bug fixes to library size variation script. Moved comments to a separate file for comments.

--- simulations/libsize.R
+++ simulations/libsize.R
@@ -9,32 +9,35 @@ dir.create("results-libsize", showWarning=FALSE)
 design <- model.matrix(~rep(0:1, each=3))
 nlibs <- nrow(design)
 
-pdf("results-libsize/twogroup.pdf")
+pdf("results-libsize/twogroup.pdf", width=10, height=5)
 par(mfrow=c(1,2))
 for (mean in c(20, 50, 100)) {
     for (disp in c(0.01, 0.05, 0.2)) {
         for (fc in c(2, 5)) { 
             means <- rep(mean, nlibs)
             means[design[,2]==1] <- mean*fc
-            makePlots(means, disp, design, equal.libs=FALSE, main=paste0("Mean = ", mean, ", dispersion = ", disp, ", FC = ", fc))
+            makePlots(means, disp, design, equal.libs=FALSE, main=paste0("Mean = ", mean, ", dispersion = ", disp, ",\n FC = ", fc))
+        }
     }
 }
 dev.off()
 
 # Additive comparison with different library sizes.
 
-design <- model.matrix(~factor(rep(0:2, each=2))+rep(0:1, 3))
+sample <- gl(3,2)
+treatment <- factor(rep(0:1, 3))
+design <- model.matrix(~sample+treatment)
 nlibs <- nrow(design)
 
-pdf("results-libsize/additive.pdf")
+pdf("results-libsize/additive.pdf", width=10, height=5)
 par(mfrow=c(1,2))
 for (mean in c(20, 50, 100)) {
     for (disp in c(0.01, 0.05, 0.2)) {
         for (fc in c(2, 5)) { 
             means <- rep(mean, nlibs)
-            means[design[,3]==1] <- mean*fc
-            makePlots(means, disp, design, equal.libs=FALSE, main=paste0("Mean = ", mean, ", dispersion = ", disp, ", FC = ", fc))
- 
+            means[design[,ncol(design)]==1] <- mean*fc
+            makePlots(means, disp, design, equal.libs=FALSE, main=paste0("Mean = ", mean, ", dispersion = ", disp, ",\n FC = ", fc))
+        }
     }
 }
 dev.off()
